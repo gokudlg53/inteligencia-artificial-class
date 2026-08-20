2a9c2e015efea63aac6981fb5adefc69e8ecd6d4
gokudlg53 <minicarlosganador@gmail.com>
2026-08-20 16:47:24 -0400

    Update Juego.py

--- dodgem/Juego.py
+++ dodgem/Juego.py
@@ -1,1 +1,14 @@
 
+tablero = [["O", ".", ".","."], ["O", ".", ".", "."], ["O", ".", ".", "."], [".", "X", "X", "X"]]
+def tablero_lleno(tablero):
+    print()
+    for fila in tablero:
+        for elem in fila:
+            print(elem, end=" ")
+        print()
+    print()
+def JUEGO():
+    tablero_lleno(tablero)
+
+    print("esto es una prueba")
+JUEGO()
