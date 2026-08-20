5ecaee4f05126eab1ab039994bf06de566ed928e
gokudlg53 <minicarlosganador@gmail.com>
2026-08-20 16:55:37 -0400

    Update Juego.py

--- dodgem/Juego.py
+++ dodgem/Juego.py
@@ -7,6 +7,19 @@ def tablero_lleno(tablero):
             print(elem, end=" ")
         print()
     print()
+def jugadas(opcion):
+    if opcion == 1:
+        tablero[i][j] = "."
+        j=j-1
+        tablero[i][j] = "X"
+    elif opcion == 2:
+        tablero[i][j] = "X"
+        i=i+1
+        tablero[i][j] = "X"
+    elif opcion == 3:
+        tablero[i][j] = "."
+        j=j+1
+        tablero[i][j] = "X"
 def JUEGO():
     tablero_lleno(tablero)
 
